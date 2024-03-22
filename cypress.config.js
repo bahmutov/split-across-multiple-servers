9fe1f64143b518b0032adc641713c97b21fd47b4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-03-22 10:21:20 -0400

    comments

--- cypress.config.js
+++ cypress.config.js
@@ -4,13 +4,17 @@ const cypressSplit = require('cypress-split')
 
 module.exports = defineConfig({
   e2e: {
-    // baseUrl, etc
+    // default URL if there is a single server
     baseUrl: 'http://localhost:3000',
     supportFile: false,
     fixturesFolder: false,
     setupNodeEvents(on, config) {
       cypressSplit(on, config)
 
+      // grab the machine index and the total number of jobs
+      // from the same variables you passed to the cypress run command
+      // in the CI workflow file (like .github/workflows/ci.yml)
+      // and determine what baseUrl to use
       const jobIndex = Number(process.env.SPLIT_INDEX)
       const jobs = Number(process.env.SPLIT)
       if (jobIndex >= 0 && jobs > 1) {
@@ -26,6 +30,8 @@ module.exports = defineConfig({
       }
 
       // IMPORTANT: return the config object
+      // with the changes you made
+      // (baseUrl plus changes made by cypress-split plugin)
       return config
     },
   },
