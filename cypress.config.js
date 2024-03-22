f975f825d8cd998d90649b21d17f6fadd0de4da9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-03-22 10:11:30 -0400

    solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,30 @@
 const { defineConfig } = require('cypress')
+// https://github.com/bahmutov/cypress-split
+const cypressSplit = require('cypress-split')
 
 module.exports = defineConfig({
   e2e: {
     // baseUrl, etc
-    baseUrl: 'http://localhost:3001',
+    baseUrl: 'http://localhost:3000',
     supportFile: false,
     fixturesFolder: false,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-      // and load any plugins that require the Node environment
+      cypressSplit(on, config)
+
+      const jobIndex = parseNumber(process.env.SPLIT_INDEX)
+      const jobs = parseNumber(process.env.SPLIT)
+      if (jobIndex >= 0 && jobs > 1) {
+        config.baseUrl = `http://localhost:300${jobIndex}`
+        console.log(
+          'Job index %d of %d, using baseUrl %s',
+          jobIndex,
+          jobs,
+          config.baseUrl,
+        )
+      }
+
+      // IMPORTANT: return the config object
+      return config
     },
   },
 })
